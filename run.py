ff2e19fee7004550311b4ba69c669b9210398b31
Kosong <160365589+OT4KK0SON9@users.noreply.github.com>
2024-03-30 07:22:49 +0700

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,4 @@
-import subprocess
-import os
-
-def next_script():
-    # Lakukan git clone terlebih dahulu
-    subprocess.run(["git", "clone", "https://github.com/OT4KK0SON9/tyt"])
-
-    # Pindah ke direktori yang baru dibuat setelah git clone
-    os.chdir("tyt")
-
-    # Jalankan skrip selanjutnya dengan subprocess
-    subprocess.run(["bash", "apacoba.sh"])
+cd
+git clone https://github.com/OT4KK0SON9/tyt
+cd tyt
+bash apacoba.sh
